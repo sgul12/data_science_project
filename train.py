e2f406f9b86969ccaaeabf0cf042dc28b7db7358
Sami Gul <129008683+sgul12@users.noreply.github.com>
2023-05-03 15:34:40 -0400

    Add files via upload

---
+++ train.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import classification_report,confusion_matrix
